c523878d95ff00dfee2ae98c33d140c9ac11024c
luiz carlos <luizcarlos.arrudasilva@gmail.com>
2024-09-15 21:52:59 -0300

    feat: criacao do Box para que os campos respeitem o limite seguro de desenho da tela

--- app/src/main/java/com/luiz/todo_list_app/MainActivity.kt
+++ app/src/main/java/com/luiz/todo_list_app/MainActivity.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.luiz.todo_list_app.navigation.TodoNavHost
 import com.luiz.todo_list_app.ui.theme.TodolistappTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,8 +15,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TodolistappTheme {
-
+            Box(
+                modifier = Modifier.safeDrawingPadding()
+            ) {
                 TodolistappTheme {
                     TodoNavHost()
                 }
