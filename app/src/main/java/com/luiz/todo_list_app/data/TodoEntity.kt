a0b710d0dcec3bf628c1b3f212bc8af846a79616
luiz carlos <luizcarlos.arrudasilva@gmail.com>
2024-09-30 15:03:59 -0300

    feat: criacao da entidade de dados

---
+++ app/src/main/java/com/luiz/todo_list_app/data/TodoEntity.kt
@@ -0,0 +1,13 @@
+package com.luiz.todo_list_app.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "todos")
+data class TodoEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long = 0,
+    val title: String,
+    val description: String?,
+    val isCompleted: Boolean,
+)
